d1704cd1bc4f14bbb7ceaa6d5caf5bf017a315ef
David Quaggin-Smith <quagginsmith@gmail.com>
2021-04-14 13:37:52 -0500

    adds artist/song relation, artist views and controller actions

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -1,2 +1,22 @@
 class Api::ArtistsController < ApplicationController
+  def show
+    id_search = params["id"]
+    @artist = Artist.find_by(id: id_search)
+    render show.json.jb
+  end
+
+  def create
+    @artist = Artist.new(
+      name: params[:name],
+      song_id: params[:song_id],
+    )
+    if @artist.save
+      render "show.json.jb"
+    else
+      render json: { errors: @artis.errors.full_message }, status: 406
+    end
+  end
+
+  def update
+  end
 end
